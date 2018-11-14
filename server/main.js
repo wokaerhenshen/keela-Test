53caee4b80c840728daf5d4e375edb85317435b1
karl <karlxu0410@gmail.com>
2018-11-14 00:07:45 -0800

    commit in 11-14

--- server/main.js
+++ server/main.js
@@ -2,4 +2,5 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  //"start": "MONGO_URL=mongodb://username:password@host_url:portnumber/dbname meteor run"
 });
