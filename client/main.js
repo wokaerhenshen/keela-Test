29b0867bef15cef4eae292bb14535379f333fbc1
karl <karlxu0410@gmail.com>
2018-11-16 19:04:10 -0800

    improve file structures, added login/register/home page with rescrict access feature . Fix the font issue . Fix the bootstrap version issue . added the icon

--- client/main.js
+++ client/main.js
@@ -1,12 +1,96 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor} from 'meteor/meteor';
+import "../imports/css/bootstrap.min.css";
+import "../imports/css/oneui.css";
+
+import "../imports/js/jquery.min.js";
+import "../imports/js/bootstrap.min.js";
+import "../imports/js/jquery.validate.min.js";
+
+import "../imports/ui/home.html";
+import "../imports/ui/login.html";
+import "../imports/ui/register.html";
 
 
 import './main.html';
+import { Accounts } from 'meteor/accounts-base';
 
 Router.route('/', {
-  template: 'register'
+  template: 'login'
 });
 
 Router.route('/register');
 Router.route('/login');
+Router.route('/home',{
+  name:'home',
+  template:'home',
+  onBeforeAction:function(){
+  var currentUser = Meteor.userId();
+  if(currentUser){
+    //logged-in
+    if(Meteor.user()){
+      var email = Meteor.user().emails[0].address;
+      this.render('home',{data:{name:email}});
+    }else {
+      this.next();
+    }
+  }else {
+    //not logged
+    this.render('login');
+  }
+}
+});
+
+if(Meteor.isClient){
+
+  Template.register.events({
+    'submit .js-validation-register':function(event){
+      event.preventDefault();
+      //alert('hi');
+      console.log('hello');
+      var userName = $('#register-username').val();
+      var email = $('#register-email').val();
+      var password = $('#register-password').val();
+      Accounts.createUser({
+        email:email,
+        password:password,
+        userName:userName
+      },function(error){
+        if(error){
+          alert(error.reason);
+        }else{
+          Router.go('home');
+        }
+      });
+      
+    }  
+  });
+
+  Template.home.events({
+    'click .logout':function(event){
+      event.preventDefault();
+      Meteor.logout();
+      Router.go('login');
+    }
+  });
+
+  Template.login.events({
+    'submit .js-validation-login':function(event){
+      event.preventDefault();
+      var email = $('#login-username').val();
+      var password = $('#login-password').val();
+      Meteor.loginWithPassword(email,password,function(error){
+        console.log("try to login...");
+        if(error){
+          alert(error.reason);
+        }else {
+          Router.go('home');
+        }
+
+      });
+
+    }
+  })
+
+
+}
--- server/main.js
+++ server/main.js
@@ -4,3 +4,8 @@ Meteor.startup(() => {
   // code to run on server at startup
   //"start": "MONGO_URL=mongodb://username:password@host_url:portnumber/dbname meteor run"
 });
+
+Accounts.onCreateUser(function(options, user) {
+  user.userName = options.userName;
+  return user;
+});
