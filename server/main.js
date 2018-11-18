e378b8770f590e4bf72cdf01dfee12c3df4ab5cf
karl <karlxu0410@gmail.com>
2018-11-18 15:19:37 -0800

    commit the env var

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
   console.log(process.env);
-  process.env.MONGO_URL='mongodb://xuwenjie410:Aa19940422~@ds211724.mlab.com:11724/keelademo';
+  //process.env.MONGO_URL='mongodb://xuwenjie410:Aa19940422~@ds211724.mlab.com:11724/keelademo';
 });
 
 Accounts.onCreateUser(function(options, user) {
